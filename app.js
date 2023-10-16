f957b86a3c730561e4a3a95f66d4c612ece171a3
Abraham TireniOluwa Olamide <121473246+Tech-Reni@users.noreply.github.com>
2023-10-17 00:11:23 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+let hamburger = document.querySelector('.hamburger')
+let mbl_nav_cont = document.querySelector('.mobile-nav-container');
+let close_btn = document.querySelector('.close');
+
+hamburger.addEventListener('click', () => {
+    mbl_nav_cont.classList.toggle('active');
+});
+
+close_btn.addEventListener('click', () => {
+    mbl_nav_cont.classList.toggle('active');
+})
+
+
+ScrollReveal({
+    reset: true,
+    distance: '100px',
+    duration: 1800,
+    delay: 100
+});
+
+ScrollReveal().reveal('.heading', { origin: 'top' });
+ScrollReveal().reveal('.dy-says , .said-by', { origin: 'bottom' });
+
+let quote1 = 'We are a WORD based church that believes in the HolySpirit';
+
+let quote2 = 'Sin is the Enterprise of the devil';
+
+let quote3 = 'Christianity is the normal way of life';
+
+const typed = new Typed('.words', {
+    strings: [quote1, quote2, quote3],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 5000,
+    loop: true,
+    cursor: 'none'
+});
